0df75ada36839d77ce6b331a857373688d09dd69
Alex <weavel.alex@gmail.com>
2021-09-14 11:38:36 +0200

    initial commit

---
+++ tests/tests-env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+##
+## tests-env.sh
+## 
+## Made by Benjamin Negrevergne
+## Login   <benjamin@neb.dyn.cs.kuleuven.be>
+## 
+## Started on  Thu Jan  3 11:48:32 2013 Benjamin Negrevergne
+## Last update Thu Jan  3 11:48:32 2013 Benjamin Negrevergne
+##
+
+export PATH=/Users/alexandre/Documents/STAGE/edit_paraminer-1.0 copie/src/:/Users/alexandre/Documents/STAGE/edit_paraminer-1.0 copie/src/:/Users/alexandre/Documents/STAGE/edit_paraminer-1.0 copie/scripts/:/Users/alexandre/Documents/STAGE/edit_paraminer-1.0 copie/tests/external/bin:/Users/alexandre/Documents/STAGE/edit_paraminer-1.0 copie/tests/:$PATH
+export PM_DATASET_PATH=/Users/alexandre/Documents/STAGE/edit_paraminer-1.0 copie/data/
+
